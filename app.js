39b85242bb425f286d698266801ee501ee42a65c
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-04 00:02:48 +0300

    Models,Routes,Controller oluşturuldu

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute")
+const courseRoute = require("./routes/courseRoute")
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('index');
+// -- CONNECT DB --
+mongoose.connect('mongodb://localhost/smartedu-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
+
+// -- TEMPLATE ENGINE --
+app.set("view engine", "ejs");
+
+// -- MIDDLEWARES --
+app.use(express.static("public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// -- ROUTES --
+app.use("/", pageRoute);
+app.use('/courses', courseRoute);
+
+
+
 
 const port = 3000;
-
-app.listen(port, () => {
-  console.log(`App started on  port ${port}`);
-});
+app.listen(port, ()=>{
+    console.log(`App started on port ${port}`)
+})
