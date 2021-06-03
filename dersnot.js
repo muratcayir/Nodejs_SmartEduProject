6924735d92ad00bdca53c391f36b2f8dcf31a580
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-03 11:03:38 +0300

    Lesson 01

---
+++ dersnot.js
@@ -0,0 +1,16 @@
+//-------------Sık Kullanılan Durum Kodları(http status code)--------
+/*
+--->Sunucu istemci arasında ki temel ilişkiyi TCP/IP sağlar
+--->http sunucu istemci arasında ki iletişim dilidir 
+---> http status code o an yapılan işlemler hakkında bilgi verir
+200 - OK
+201 – Created
+404 – Not found
+500 – Internal Server Error
+201 – Created
+400 – Bad Request
+401 – Unauthorized
+403 – Forbidden
+501 – Not Implemented
+
+*/
