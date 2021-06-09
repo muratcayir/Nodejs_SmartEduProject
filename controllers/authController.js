c58ca9d4afc2b63fbda618d3c2d0aaf2089461e9
Murat ÇAYIR <mrtcyr12@hotmail.com>
2021-06-09 23:25:56 +0300

    The course was added by which user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
-const bcrypt= require('bcrypt')
+const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
-
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
   try {
@@ -25,7 +25,7 @@ exports.loginUser = async (req, res) => {
         bcrypt.compare(password, user.password, (err, same) => {
           if (same) {
             // USER SESSION
-            req.session.userID=user._id;
+            req.session.userID = user._id;
             res.status(200).redirect('/users/dashboard');
           }
         });
@@ -39,21 +39,21 @@ exports.loginUser = async (req, res) => {
   }
 };
 
-
-exports.logoutUser= (req,res)=>
-{
-  req.session.destroy(()=>
-  {
-    res.redirect('/')
+exports.logoutUser = (req, res) => {
+  req.session.destroy(()=> {
+    res.redirect('/');
   })
 }
 
+
 exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({_id:req.session.userID})
-  const categories = await Category.find()
+  const categories = await Category.find();
+  const courses = await Course.find({user:req.session.userID})
   res.status(200).render('dashboard', {
     page_name: 'dashboard',
     user,
-    categories
+    categories,
+    courses
   });
 };
